7ebe03b3cbef5c98600e019be4b7c54ab1f06175
Jeff Prestes <jeffprestes@gmail.com>
2018-02-28 13:43:41 -0300

    Ajuste nas traduções

--- eth/backend.go
+++ eth/backend.go
@@ -117,7 +117,7 @@ func New(ctx *node.ServiceContext, config *Config) (*Ethereum, error) {
 	if _, ok := genesisErr.(*params.ConfigCompatError); genesisErr != nil && !ok {
 		return nil, genesisErr
 	}
-	log.Info("Initialised chain configuration", "config", chainConfig)
+	log.Info("Inicializando a configuração da cadeia de blocos", "config", chainConfig)
 
 	eth := &Ethereum{
 		config:         config,
@@ -135,7 +135,7 @@ func New(ctx *node.ServiceContext, config *Config) (*Ethereum, error) {
 		bloomIndexer:   NewBloomIndexer(chainDb, params.BloomBitsBlocks),
 	}
 
-	log.Info("Initialising Ethereum protocol", "versions", ProtocolVersions, "network", config.NetworkId)
+	log.Info("Inicializando o protocolo Ethereum", "versões", ProtocolVersions, "rede", config.NetworkId)
 
 	if !config.SkipBcVersionCheck {
 		bcVersion := core.GetBlockChainVersion(chainDb)
--- les/backend.go
+++ les/backend.go
@@ -88,7 +88,7 @@ func New(ctx *node.ServiceContext, config *eth.Config) (*LightEthereum, error) {
 	if _, isCompat := genesisErr.(*params.ConfigCompatError); genesisErr != nil && !isCompat {
 		return nil, genesisErr
 	}
-	log.Info("Initialised chain configuration", "config", chainConfig)
+	log.Info("Inicializando a configuração da cadeia de blocos", "config", chainConfig)
 
 	peers := newPeerSet()
 	quitSync := make(chan struct{})
